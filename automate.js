603dcf35a9abab8b4828efddb8ecb4d06c99313d
Vedant bhavsar <vedantbhavsar.a10@gmail.com>
2024-05-25 20:20:59 +0530

    optimizations

--- automate.js
+++ automate.js
@@ -1,23 +1,73 @@
 const { default: axios } = require('axios');
 
-let page = 47;
-async function fetching() {
-  const { data } = await axios.get(
-    `http://localhost:3001/anime/popular?page=${page}`
+// let page = 47;
+// async function fetching() {
+//   const { data } = await axios.get(
+//     `http://localhost:3001/anime/popular?page=${page}`
+//   );
+
+//   data?.forEach(async (element) => {
+//     await axios
+//       .get(`http://localhost:3001/anime/fetch/${element.id}`)
+//       .catch((error) => console.log(error.message));
+//   });
+
+//   if (page < 100) {
+//     page++;
+//     setTimeout(() => {
+//       fetching();
+//     }, 3000);
+//   }
+// }
+
+// // fetching();
+
+
+// async function reverse_proxy(){
+//   const {data} = await axios.get('https://embtaku.pro/download?id=MjEzMDA4&token=PPQjf03A8ISCJD0BerC9iA&expires=1716481205').catch(error => console.log(error.message))
+//   console.log(data)
+// }
+
+
+// reverse_proxy()
+
+
+
+const puppeteer = require('puppeteer')
+
+
+const Nod = async()=>  {
+  // Launch the browser and open a new blank page
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+
+  // Navigate the page to a URL
+  await page.goto('https://embtaku.pro/download?id=MjEzMDA4&token=PPQjf03A8ISCJD0BerC9iA&expires=1716481205');
+
+  // Set screen size
+  await page.setViewport({width: 1080, height: 1024});
+
+  // Type into search box
+  // await page.type('.devsite-search-field', 'automate beyond recorder');
+
+  // Wait and click on first result
+  const searchResultSelector = '.download';
+  console.log(searchResultSelector)
+  await page.waitForSelector(searchResultSelector);
+  console.log(page)
+  await page.click(searchResultSelector);
+
+  // Locate the full title with a unique string
+  const textSelector = await page.waitForSelector(
+    'text/Customize and automate'
   );
+  const fullTitle = await textSelector?.evaluate(el => el.textContent);
 
-  data?.forEach(async (element) => {
-    await axios
-      .get(`http://localhost:3001/anime/fetch/${element.id}`)
-      .catch((error) => console.log(error.message));
-  });
+  // Print the full title
+  console.log('The title of this blog post is "%s".', fullTitle);
 
-  if (page < 100) {
-    page++;
-    setTimeout(() => {
-      fetching();
-    }, 3000);
-  }
-}
+  await browser.close();
+};
 
-fetching();
+
+Nod()
--- src/routes/newsRoutes.ts
+++ src/routes/newsRoutes.ts
@@ -1,10 +1,27 @@
-import { Router } from 'express';
-import NewsController from '../controllers/news';
+import { Router } from "express";
+import NewsController from "../controllers/news";
+import axios from "axios";
 
 const router = Router();
 
 // Routes for accessing news
-router.get('/fetch', NewsController.fetchNewsFromDB);
-router.get('/fetch-feeds', NewsController.fetchNewsFeeds);
+router.get("/fetch", NewsController.fetchNewsFromDB);
+router.get("/fetch-feeds", NewsController.fetchNewsFeeds);
+router.get("/download", async (req, res) => {
+  const data: any = await axios
+    .request({
+      method: "GET",
+      url: "https://embtaku.pro/download?id=MjEzMDA4&token=PPQjf03A8ISCJD0BerC9iA&expires=1716481205",
+      headers: {
+        "User-Agent":
+          "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/125.0.0.0 Safari/537.36",
+        "Sec-Ch-Ua": `"Brave";v="125", "Chromium";v="125", "Not.A/Brand";v="24"`,
+        "Sec-Ch-Ua-Platform": "Linux",
+      },
+    })
+    .catch((error: Error) => console.log(error.message));
+  console.log(data);
+  res.send(data?.data);
+});
 
 export { router as NewsRoute };
